d70c60a710a8f2de45c4dae638c0faff63e1789e
Andrey Chernykh <achernykh@ethoslending.com>
2019-10-14 22:35:34 +0300

    explorer

---
+++ algoritms/hw1/explorer/engine/Explorer.php
@@ -0,0 +1,24 @@
+<?php
+    namespace app\engine;
+
+    class Explorer {
+        public static function getDirectoryIterator(string $path) {
+            return new \DirectoryIterator($path);
+        }
+
+        public static function getFolders(string $path) {
+            $di = static::getDirectoryIterator($path);
+            foreach ($di as $file) {
+                if(!$file->isDir()) continue;
+                yield $file;
+            }
+        }
+
+        public static function getFiles(string $path) {
+            $di = static::getDirectoryIterator($path);
+            foreach ($di as $file) {
+                if(!$file->isFile()) continue;
+                yield $file;
+            }
+        }
+    }
---
+++ algoritms/hw1/explorer/public/index.php
@@ -0,0 +1,52 @@
+<?php
+    include "../config/config.php";
+    include "../engine/Autoload.php";
+
+    use app\engine\{Autoload, Explorer};
+
+    spl_autoload_register([new Autoload(), 'loadClass']);
+
+    if (!$_GET['path']) {
+        $path = 'C:\\';
+    } else {
+        $path = $_GET['path'];
+    }
+
+?>
+
+<b>Explorer</b>
+<?php foreach (Explorer::getFolders($path) as $f): ?>
+    <p>
+        <form method="get" action="/.">
+            <input type="hidden" name="path" value="<?=$f->getPathname()?>">
+            <input type="submit" class="linkButton" value="<?=$f->getFileName()?>" />
+        </form>
+    </p>
+<?php endforeach;?>
+
+<?php foreach (Explorer::getFiles($path) as $f): ?>
+    <p><?=$f->getFileName()?></p>
+<?php endforeach;?>
+
+
+<style>
+    b {
+        font-size: 28px;
+    }
+
+    p, form, .linkButton {
+        margin: 0;
+    }
+
+    p {
+        padding-left: 6px;
+    }
+
+    .linkButton {
+        background: none;
+        border: none;
+        color: #0066ff;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
