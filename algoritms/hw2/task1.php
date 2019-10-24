fc2ae129087ae1efba965e188bdbbd62e943656f
Andrey Chernykh <achernykh@ethoslending.com>
2019-10-24 17:25:22 +0300

    task 1

--- algoritms/hw2/task1.php
+++ algoritms/hw2/task1.php
@@ -4,6 +4,12 @@
     // сначала нашел алгоритм и пытался реализовать ситом Эратосфена
     // после 1000000 вычисления уходили в бесконечность
     // потом нашел идею вот такова супер крутого алгоритма
+
+    //Example: Let's find the largest prime factor of 105.
+    //Let (A) = 105. (B) = 2 (we always start with 2), and we don't have a value for (C) yet.
+    //Is (A) divisible by (B)? No. Increment (B) by +1: (B) = 3. Is Is (A) divisible by (B)?
+    //Yes. (105/3 = 35). The largest divisor found so far is 3. Let (C) = 3. Update (A) = 35. Reset (B) = 2...
+
     function getMaxPrimeDivisor(int $number) {
         $a = $number;
         $b = SMALLEST_DIVISOR;
